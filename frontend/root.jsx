78ff1ee995a4b56e9d210cbecb8b4a046d561546
SONIK JHANG <sonikjhang@SONIKs-MacBook-Pro.local>
2017-04-25 15:15:07 -0700

    initial push

---
+++ frontend/root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root/>, document.getElementById('main'));
+});
