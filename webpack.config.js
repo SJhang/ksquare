13133221ae85700bf202bda3b30db28b8d0eb947
SONIK JHANG <sonikjhang@SONIKs-MacBook-Pro.local>
2017-05-16 15:14:49 -0700

    renew css

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,7 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
-      },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      }
     ]
   },
   resolve: {
