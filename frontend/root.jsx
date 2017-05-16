8d51d9d3db6a260d326fd104e5ca36cd44c46fb8
SONIK JHANG <sonikjhang@SONIKs-MacBook-Pro.local>
2017-05-16 15:21:45 -0700

    solve merge conflict

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -9,9 +9,6 @@ import $ from 'jquery';
 // ajax call
 import { fetchUsers } from './utils';
 
-//sass import
-require('../stylesheets/application.scss');
-
 class Root extends React.Component {
   constructor(props) {
     super(props);
